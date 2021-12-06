4aaa16630f0c8ee6b2077e6267724e973c446ef1
Ravil Badamshin <rsbadamshin@gmail.com>
2021-12-06 22:54:39 +0300

    add pattern attribute to form input element

--- sort_comments/views.py
+++ sort_comments/views.py
@@ -6,15 +6,16 @@ import re
 
 # Create your views here.
 def index(request):
-    base_url = "http://habr.com"
     if request.method == 'POST':
-        if re.match(r'^\b(https://)?habr.com(/\w+)+/', request.POST['habr_url']):
-            base_url = request.POST['habr_url']
-            if not base_url.endswith('comments/', -8):
-                if base_url.endswith('/', -1):
-                    base_url = base_url + 'comments/'
-                else:
-                    base_url = base_url + '/comments/'
+        base_url = request.POST['habr_url']
+        if not base_url.endswith('comments/', -9):
+            if base_url.endswith('/', -1):
+                base_url = base_url + 'comments/'
+            else:
+                base_url = base_url + '/comments/'
+    else:
+        base_url = "https://habr.com/ru/post/592135/"
+        return render(request, "index.html", {'habr_url': base_url})
 
     r = requests.get(base_url)
     c = r.content
@@ -28,9 +29,10 @@ def index(request):
             comment['username'] = article.find("a", {"class": "tm-user-info__userpic"}).get('title')
             comment['user_url'] = "https://habr.com" + article.find("a", {"class": "tm-user-info__username"})['href']
             comment['time'] = article.find("a", {"class": "tm-comment-thread__comment-link"}).get_text().strip()
-            comment['raw_comment'] = str(article.find("div", {"class": "tm-comment__body-content"}))
-            comment['comment_url'] = base_url + article.find("a", {"class": "tm-comment-thread__comment-link"}).get(
-                'href')
+            comment['raw_comment'] = str(article.find("div",
+                                                      {"class": "tm-comment__body-content"})).replace("<br/>", "")
+            comment['comment_url'] = base_url + article.find("a",
+                                                             {"class": "tm-comment-thread__comment-link"}).get('href')
             comment['score'] = article.find("span", {"class": "tm-votes-meter__value_rating"}).get_text()
             comment['score'] = int(comment['score'].replace("+", "").replace("–", "-"))
             votes = re.findall(r'↑\d+|↓\d+',
@@ -46,4 +48,4 @@ def index(request):
     elif request.POST.get('Sort By:') == 'controversial':
         all_comments.sort(key=lambda d: d['controversy'], reverse=True)
 
-    return render(request, "index.html", {'comments': all_comments})
+    return render(request, "index.html", {'comments': all_comments, 'habr_url': base_url[:-9]})
